76b37176c65707afaba3702141ca260dcf5296de
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:03:52 +0530

    Create 02_variables_and_data_types.py

---
+++ 02_variables_and_data_types.py
@@ -0,0 +1,28 @@
+--Notes
+
+Basic Python data types:
+
+Type	Example
+int	27
+float	6.9
+str	"Suraj"
+bool	True / False
+None	None
+
+Check type:
+
+print(type(age))
+
+
+a=3
+b=3
+c=a*b
+
+a=1 #int
+b=1.2 #float
+c="suraj" #String
+d=True #boolean
+e=False #Boolean
+f=None #none type
+g=""  #string blank
+h=" " #String empty space
