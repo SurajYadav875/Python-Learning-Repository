c775437d346b9a2be607fe4604f6f01133ea6b65
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:13:30 +0530

    Create 08_string_cleaning.py

---
+++ 08_string_cleaning.py
@@ -0,0 +1,25 @@
+#Whitespace cleanup
+is_clean=len(Course1)==len(Course1.strip())
+Topics
+strip()
+lstrip()
+rstrip()
+Removing special characters
+Checking for whitespace
+Data cleaning
+Notes
+text.strip()
+
+removes leading and trailing whitespace.
+
+text.lstrip()
+
+removes whitespace from the left.
+
+text.rstrip()
+
+removes whitespace from the right.
+
+This is very important for Data Analyst work because real-world datasets often contain unwanted spaces.
+
+  
