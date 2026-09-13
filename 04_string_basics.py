fe64f0a881c896ffc97604e1e5188d6643e4c882
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:05:45 +0530

    Create 04_string_basics.py

---
+++ 04_string_basics.py
@@ -0,0 +1,23 @@
+Topics
+Strings
+len()
+count()
+
+Notes
+
+len() → number of characters.
+
+name = "Suraj"
+print(len(name))
+
+count() → counts occurrences.
+
+text = "Python Python"
+print(text.count("Python"))
+
+
+name='suraj'
+password="12348533533"
+text="""..."""
+print(len(password))
+print(text.count('$'))
