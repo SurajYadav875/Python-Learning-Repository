6a73ccf18bad0db4e7268f387856e5e8ff0a5eca
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:02:41 +0530

    Create 01_print_and_comments.py

---
+++ 01_print_and_comments.py
@@ -0,0 +1,23 @@
+--Notes
+
+---Topics covered:
+
+print()
+Escape characters
+\n → new line
+\t → tab
+\b → backspace
+Triple quotes """ """
+Comments using #
+
+Example:
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+
+
+print("Hi,this is my first code in python")
+print("""your learning path:
+...
+-ai""")
