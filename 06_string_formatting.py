3d7f0abb0aeb179d115f64e1e9060dc4297e508c
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:11:10 +0530

    Create 06_string_formatting.py

---
+++ 06_string_formatting.py
@@ -0,0 +1,23 @@
+Notes
+
+You learned two ways to combine strings.
+Firstname='Sri'
+LastName='Lanka'
+Full_Name=Firstname +' ' + LastName
+print(Full_Name)
+
+first_name='Suraj'
+last_name='yadav'
+old=27
+is_student=False
+
+Concatenation
+Full_Name = first_name + " " + last_name
+f-string 
+print(
+    f"My name is {first_name} {last_name}, "
+    f"I am {old} years old, "
+    f"and student status is {is_student}"
+)
+
+For modern Python, f-strings are generally the cleaner approach.
