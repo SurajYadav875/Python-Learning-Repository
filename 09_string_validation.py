d4a88adb521c5f0169101eadbb5665671fd28195
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:14:28 +0530

    Create 09_string_validation.py

---
+++ 09_string_validation.py
@@ -0,0 +1,35 @@
+#Search-String Function
+
+print(phone.isnumeric())
+Topics
+startswith()
+endswith()
+in
+find()
+isalpha()
+isnumeric()
+Notes
+phone.startswith("+91")
+
+checks whether a string starts with something.
+
+email.endswith("@outlook")
+
+checks whether a string ends with something.
+
+"8788" in phone
+
+checks whether text exists inside another string.
+
+phone.find("-")
+
+returns the position of -.
+
+Validation
+country.isalpha()
+
+Checks whether characters are alphabetic.
+
+phone.isnumeric()
+
+Checks whether characters are numeric.
