03693cfa0a54213dad81a187e5da2b762d3071ea
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:04:50 +0530

    Create 03_type_conversion.py

---
+++ 03_type_conversion.py
@@ -0,0 +1,17 @@
+
+Notes
+
+Type conversion means changing one data type into another.
+
+age = 27
+
+str(age)      # int → string
+int("27")     # string → int
+float("27")   # string → float
+
+Important for combining strings and numbers:
+
+print("My age is " + str(age))
+
+age=27
+print(type(age))
