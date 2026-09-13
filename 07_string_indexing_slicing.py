6f76e4131695129208e0c85f73cc1ca044a45513
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:12:31 +0530

    Create 07_string_indexing_slicing.py

---
+++ 07_string_indexing_slicing.py
@@ -0,0 +1,28 @@
+#Split
+a='12-44-66-434-332-55'
+
+Topics
+split()
+Indexing
+Slicing
+Negative indexing
+Notes
+
+split():
+
+a = "12-44-66"
+
+print(a.split("-"))
+
+Result:
+
+['12', '44', '66']
+Indexing
+text = "python"
+
+print(text[0])    # p
+print(text[-1])   # n
+Slicing
+text[1:4]
+
+gets characters from index 1 up to, but not including, index 4.
