1e8f6fe1fc79255d3a5841dd0c5788741ccfddfe
Suraj Yadav <76196745+SurajYadav875@users.noreply.github.com>
2026-09-13 23:15:25 +0530

    Create 10_user_input.py

---
+++ 10_user_input.py
@@ -0,0 +1,17 @@
+name=input("What is your name")
+country=input("In which country you lived?")
+print(name,"live in",country)
+
+Notes
+
+input() allows the user to enter data.
+
+name = input("What is your name? ")
+
+⚠️ Important:
+
+input() always returns a string.
+
+If you want a number:
+
+age = int(input("Enter your age: "))
